2eac9f018fa6db569758067efef6a2d10e047dcf
Leo Pathu <leopathu@gmail.com>
2025-09-17 22:33:27 +0530

    Removed unnecessary files

--- frontend/src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from './AuthContext';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children }) {
-  const { user, loading } = useContext(AuthContext);
-  if (loading) return <div>Loading...</div>;
-  return user ? children : <Navigate to="/login" />;
-}
